fc0fa1086540cbe91f3868875c7f06c736e6a5a4
Nastia Shulgina <anastasiia.shulhina.knm.2020@edu.lpnu.ua>
2021-10-14 10:06:30 +0300

    first commit

---
+++ js/dom_util.js
@@ -0,0 +1,50 @@
+const makerInput = document.getElementById("maker");
+const heightInput = document.getElementById("height");
+const materialInput = document.getElementById("material");
+const priceInput = document.getElementById("price");
+const itemsContainer = document.getElementById("items_container");
+
+
+const getItemId = (id) => `item-${id}`;
+
+const itemTemplate = ({ id, maker, height, material, price }) => `
+<li id="${getItemId(id)}" class="card">
+    <div>
+        <br><p><i>Maker's name:</i><br><br>"${maker}"</p><br>
+        <p><i>Height in CMs:</i><br><br>${height}</p><br>
+        <p><i>Material:</i><br><br>${material}</p><br>
+        <p><i>Price UAH:</i><br><br>${price}</p>
+        <button class="button__edit">Edit</button>
+    </div>
+</li>`;
+
+export const clearInputs = () => {
+    makerInput.value = "";
+    heightInput.value = "";
+    materialInput.value = "";
+    priceInput.value = "";
+};
+
+export const addItemToPage = ({ id, maker, height, material, price }) => {
+    itemsContainer.insertAdjacentHTML(
+        "afterbegin",
+        itemTemplate({ id, maker, height, material, price })
+        );
+};
+
+export const getInputValues = () => {
+    return {
+        maker: makerInput.value,
+        height: heightInput.value,
+        material: materialInput.value,
+        price: priceInput.value,
+    };
+};
+
+export const renderItemsList = (trees) => {
+    itemsContainer.innerHTML = "";
+
+    for (const tree of trees) {
+        addItemToPage(tree);
+    }
+};
---
+++ js/main.js
@@ -0,0 +1,96 @@
+import {
+    getInputValues,
+    addItemToPage,
+    clearInputs,
+    renderItemsList,
+} from "./dom_util.js";
+
+// const resetButton = document.getElementById("reset-button");
+const submitButton = document.getElementById("submit-button");
+const searchButton = document.getElementById("search-button");
+const searchInput = document.getElementById("search");
+const cancelSearchButton = document.getElementById("cancel-search-button");
+const sortButton = document.getElementById("sort-button");
+const cancelSortButton = document.getElementById("cancel-sort-button");
+const countButton = document.getElementById("count-button");
+const makerInput = document.getElementById("maker");
+const heightInput = document.getElementById("height");
+const materialInput = document.getElementById("material");
+const priceInput = document.getElementById("price");
+
+let trees = [];
+
+const addItem = ({maker, height, material, price}) => {
+    const generatedId = uuid.v1();
+
+    const newItem = {
+        id: generatedId,
+        
+        maker: maker,
+        height: height,
+        material: material,
+        price: price,
+    }
+
+    trees.push(newItem);
+    addItemToPage(newItem);
+}
+
+submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const invalidSymbols = ["!", ".", "?", ";", "~", "&", "$", "№", "<", ">", "/", "|", "\\", "#", "@", "%", "*", "₴", "`"];
+
+    if (makerInput.value == 0 || heightInput.value == 0 || materialInput.value ==  0 || priceInput.value == 0) {
+        alert("All fields must be filled!");
+    } 
+    else if (invalidSymbols.some(symbol => makerInput.value.includes(symbol))) {
+        alert("Please, write the valid maker's name. Don't use special symbols.")
+    } 
+    else if(invalidSymbols.some(symbol => materialInput.value.includes(symbol)))  {
+        alert("Please, write the valid material. Don't use special symbols.")
+    } 
+    else if(priceInput.value <= 0) {
+        alert("Would you like to earn a negative amount of money? :)\nPlease, write numbers higher than 0 next to \"Price UAH:\"")
+    } 
+    else if(heightInput.value <= 0 || heightInput.value > 300) {
+        alert("Is your tree a magic one? or a giant? :)\nPlease, write numbers higher than 0 and less than (or equal) 300 next to \"Height in CMs:\"")
+    } 
+    else {
+        const inputValues = getInputValues();
+        const {maker, height, material, price} = inputValues;
+        clearInputs();
+        addItem({maker, height, material, price});
+    }
+})
+
+searchButton.addEventListener("click", () => {
+    const foundTrees = trees.filter(tree => tree.material.search(searchInput.value) !==-1);
+    renderItemsList(foundTrees);
+})
+
+cancelSearchButton.addEventListener("click", () => {
+    renderItemsList(trees);
+    searchInput.value = "";
+})
+
+sortButton.addEventListener("click", () => {
+    const sortedTrees = trees.concat().sort((a, b) =>  b.price - a.price);
+    renderItemsList(sortedTrees);
+})
+
+cancelSortButton.addEventListener("click", () => {
+    renderItemsList(trees);
+})
+
+countButton.addEventListener("click", () => {
+    let treesPrices = trees.map((tree) => {
+        return tree.price;
+    });
+    let totalPrice = treesPrices.reduce((s, v) => s + +v, 0);
+    console.log(totalPrice);
+
+    alert("Total price of the artificial trees added is: " + totalPrice + " UAH.\nMerry Christmas!");
+})
+
+renderItemsList(trees);
